3b8eff4e9c746a8215433a50a31bd305d5962ed4
sophia <1224275483@qq.com>
2025-05-26 23:38:23 +0100

    beam hardening correction

--- ct_calibrate.py
+++ ct_calibrate.py
@@ -2,6 +2,7 @@ import numpy as np
 import scipy
 from scipy import interpolate
 from attenuate import attenuate
+from ct_detect import ct_detect
 
 def ct_calibrate(photons, material, sinogram, scale):
 
@@ -22,10 +23,18 @@ def ct_calibrate(photons, material, sinogram, scale):
 	air_coeff = material.coeffs[air]
 
 	depth = 2 * n * scale # for the fixed distance between source and detector
-
+	print('depth',depth)
 	# calculate air attenuated energy (calibration) for each energy level
 	source_total = attenuate(photons, air_coeff, depth)
 
 	# normalise the energy to total attenuation coefficient
 	sinogram = - np.log(sinogram / sum(source_total))
+	#beam hardening correction
+	depths = np.arange(0, 100, 0.1)
+	water_idx = material.name.index('Water')
+	water_coeff = material.coeffs[water_idx]
+	I_tot = ct_detect(photons, water_coeff, depths)
+	p_w = -np.log(I_tot / np.sum(source_total))
+	f = interpolate.interp1d(p_w, depths)
+	sinogram = f(sinogram)
 	return sinogram
